e50495d8c7779897be6799f5f583f119304e0598
Omri Webber <omriwebber@gmail.com>
2022-05-27 10:44:24 +1200

    cleaning up

--- static/js/crypto/crypto.js
+++ static/js/crypto/crypto.js
@@ -2,8 +2,6 @@ $( document ).ready(function() {
     user_name = $('.user-name').text().slice(1,2);
     $('.user-letter').text(user_name.toUpperCase());
 
-    var key = '780b5f3168c0a615facbc2e883c16b7599bfa908';
-    console.log(coin);
     fetch("https://api.coingecko.com/api/v3/coins/" + coin)
         .then(response => response.json())
         .then(result => displayData(result))
